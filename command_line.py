009aba5d1cb042b3e72cbec14ffe5a211ff84f94
shalimmontes <shalimmontesh@gmail.com>
2025-01-22 17:06:09 -0600

    Working on reading command line arguments

--- command_line.py
+++ command_line.py
@@ -2,6 +2,20 @@ import sys
 
 def main():
 
+    args = sys.argv[1:]
+
+    if len(args) != 4:
+        print(f"Usage: {sys.argv[0]} --disaster <kind of disaster(s)> --county <us_county>")
+        
+
+    # counter = 0
+    # for arg in args:
+    #     counter = counter + 1
+    #     print(arg)
+
+    # print(counter)
+
+    #print(args)
 
     return 0
 
