9e9a532deb33ca6eb9a84c3e1f34eea08879f673
shalimmontes <shalimmontesh@gmail.com>
2025-01-22 18:43:06 -0600

    Added comments and docstrings

--- ProductionCode/helper.py
+++ ProductionCode/helper.py
@@ -1,6 +1,10 @@
 import csv, sys
 
 def is_disaster(string):
+    ''' Arguments: Takes in a disaster as a string
+    Returns: True or False
+    Purpose: Checks to see if disaster inputted is a disaster in the data
+    '''
     valid_disaster_list = ["avalanche", "coastal flooding", "cold wave", "drought", "earthquake", "hail",
                            "heat wave", "hurricane", "ice storm", "landslide", "lightning", "riverine flooding", 
                            "strong wind", "tornado", "tsunami", "volcanic activity", "wildfire", "winter weather"]
@@ -11,7 +15,10 @@ def is_disaster(string):
         return True
 
 def is_us_county(county):
-
+    ''' Arguments: Takes in a county as a string
+    Returns: True or False
+    Purpose: Checks to see if county inputted is in the US and thus in the data
+    '''
     with open('Data/County_and_Disasters_only.csv', 'r') as file:
         disasterdata = csv.reader(file)
         for row in disasterdata:
--- command_line.py
+++ command_line.py
@@ -9,12 +9,12 @@ def main():
     # Checks if Command Line input is valid
     if ((args[0] != "--disaster" and args[0] != "--disasters") or args[2] != "--county" or len(args) != 4):
         print(f"Usage: {sys.argv[0]} --disaster or --disasters <kind of disaster(s)> --county <us_county>")
-        
         exit(1)
 
     disaster = args[1]
     county = args[3]
 
+    # Testing purposes
     if (is_us_county(county) == False):
         print("Not a valid county")
     else:
