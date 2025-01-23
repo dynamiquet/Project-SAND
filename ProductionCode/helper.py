984db6086e6cbd2d818dca02d5646c0ccd15d2ac
Francisco <farenas9134@gmail.com>
2025-01-22 21:12:53 -0600

    fixed some indentation stuff

--- ProductionCode/helper.py
+++ ProductionCode/helper.py
@@ -30,8 +30,6 @@ def is_us_county(county):
 
 def get_int_rating(table_rating):
     # Integer ratings for data hazards
-
-
     if (table_rating == 'Insufficient Data'):
         int_rating = 0
     elif (table_rating == 'Not Applicable'):
@@ -48,16 +46,15 @@ def get_int_rating(table_rating):
         int_rating = 6
     elif (table_rating == 'Very High'):
         int_rating = 7
+    # Debugging purposes
     else:
         print(table_rating)
-        print("Error not valid")
+        print("Error, not a valid rating")
 
     return int_rating
 
 def get_string_rating(int_rating):
     # Revert ratings for back into data hazards rating labels
-
-
     if (int_rating == 0):
         string_rating = 'Insufficient Data'
     elif (int_rating == 1):
@@ -86,61 +83,59 @@ def get_top_five(county):
             if str(row_data[0]).lower() == county.lower():
                 # Store target county row data
                 county_data = row_data[0:]
+    file.close()
 
-        #Initialize empty dictionary of top five disasters
-        top_five_disasters = {}
+    # Isolate hazard ratings, remove county and general rating
+    county_data.pop(0)
+    county_data.pop(0)
 
-        # Isolate hazard ratings, remove county and general rating
-        county_data.pop(0)
-        county_data.pop(0)
+    # Variable to keep track of where in list of county data we are at
+    county_data_pos = 0
 
-        # Variable to keep track of where in list of county data we are at
-        county_data_pos = 0
+    # Initialize disaster rating dictionary with disasters in order
+    disaster_rating_dict = {
+        "avalanche" : 0,
+        "coastal flooding" : 0,
+        "cold wave" : 0,
+        "drought" : 0,
+        "earthquake" : 0,
+        "hail" : 0,
+        "heat wave" : 0,
+        "hurricane": 0,
+        "ice storm": 0,
+        "landslide": 0,
+        "lightning" : 0,
+        "riverine flooding" : 0,
+        "strong wind" : 0,
+        "tornado" : 0,
+        "tsunami" : 0,
+        "volcanic activity" : 0,
+        "wildfire" : 0,
+        "winter weather" : 0
+    }
 
-        # Initialize disaster rating dictionary with disasters in order
-        disaster_rating_dict = {
-            "avalanche" : 0,
-            "coastal flooding" : 0,
-            "cold wave" : 0,
-            "drought" : 0,
-            "earthquake" : 0,
-            "hail" : 0,
-            "heat wave" : 0,
-            "hurricane": 0,
-            "ice storm": 0,
-            "landslide": 0,
-            "lightning" : 0,
-            "riverine flooding" : 0,
-            "strong wind" : 0,
-            "tornado" : 0,
-            "tsunami" : 0,
-            "volcanic activity" : 0,
-            "wildfire" : 0,
-            "winter weather" : 0
-        }
+    # Depending on rating, assign numerical value for later sorting
+    for disaster in disaster_rating_dict:
 
-        # Depending on rating, assign numerical value for later sorting
-        for disaster in disaster_rating_dict:
+        county_disaster_rating = get_int_rating(county_data[county_data_pos])
 
-            county_disaster_rating = get_int_rating(county_data[county_data_pos])
+        disaster_rating_dict.update({disaster : county_disaster_rating})
 
-            disaster_rating_dict.update({disaster : county_disaster_rating})
+        county_data_pos += 1
 
-            county_data_pos += 1
+    # Sort disasters by hazard rating in ascending order
+    sorted_disasters_dict = dict(reversed(sorted(disaster_rating_dict.items(), key=lambda item: item[1])))
 
-        # Sort disasters by hazard rating in ascending order
-        sorted_disasters_dict = dict(reversed(sorted(disaster_rating_dict.items(), key=lambda item: item[1])))
+    # Revert numerical values back to string value for user readability
+    for disaster in sorted_disasters_dict:
 
-        # Revert numerical values back to string value for user readability
-        for disaster in sorted_disasters_dict:
+        disaster_string_rating = get_string_rating(sorted_disasters_dict.get(disaster))
 
-            disaster_string_rating = get_string_rating(sorted_disasters_dict.get(disaster))
+        sorted_disasters_dict.update({disaster : disaster_string_rating})
 
-            sorted_disasters_dict.update({disaster : disaster_string_rating})
-
-        # Pop lowest disasters from dictionary until 5 remain
-        for int in range(0,13):
-            sorted_disasters_dict.popitem()
+    # Pop lowest disasters from dictionary until 5 remain
+    for int in range(0,13):
+        sorted_disasters_dict.popitem()
 
     return sorted_disasters_dict
 
