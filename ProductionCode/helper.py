df3264cf7daf945c1f19ee1ad56789273791ac80
shalimmontes <shalimmontesh@gmail.com>
2025-01-22 18:05:47 -0600

    Made some helper functions in helper.py

--- ProductionCode/helper.py
+++ ProductionCode/helper.py
@@ -1,2 +1,22 @@
 import csv, sys
 
+def valid_disaster(string):
+    valid_disaster_list = ["avalanche", "coastal flooding", "cold wave", "drought", "earthquake", "hail",
+                           "heat wave", "hurricane", "ice storm", "landslide", "lightning", "riverine flooding", 
+                           "strong wind", "tornado", "tsunami", "volcanic activity", "wildfire", "winter weather"]
+    
+    return 0
+
+def valid_county(county):
+
+    with open('Data/County_and_Disasters_only.csv', 'r') as file:
+        disasterdata = csv.reader(file)
+        for row in disasterdata:
+            print(row[0])
+            if row[0] == county:
+                return True
+            else:
+                return False
+    
+    file.close()
+    
--- command_line.py
+++ command_line.py
@@ -9,8 +9,12 @@ def main():
     if (args[0] != "--disaster" or args[2] != "--county" or len(args) != 4):
         print(f"Usage: {sys.argv[0]} --disaster <kind of disaster(s)> --county <us_county>")
         exit(1)
-    
 
+    disaster = args[1]
+    county = args[3]
+
+    if (valid_county(county) == False):
+        print("Not a valid county")
     # print(counter)
 
     #print(args)
