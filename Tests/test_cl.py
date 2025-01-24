5701110f9dfebba41715b223a9e39e906ab24cea
shalimmontes <shalimmontesh@gmail.com>
2025-01-24 17:43:19 -0600

    Changed a couple of unittests

--- Tests/test_cl.py
+++ Tests/test_cl.py
@@ -1,14 +1,13 @@
 import unittest, subprocess
-
 from ProductionCode.helper import *
 
 class ProjectMethodsTests(unittest.TestCase):
     def test_is_disaster(self):
         test1 = is_disaster("tornado")
-        self.assertEqual(test1, "True")
+        self.assertEqual(test1, True)
 
         edgetest1 = is_disaster("TORNADO")
-        self.assertEqual(edgetest1, "True")
+        self.assertEqual(edgetest1, True)
 
         test2 = is_disaster("United States")
-        self.assertEqual(test2, "False")
+        self.assertEqual(test2, False)
