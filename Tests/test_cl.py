9b88c8e8878068ce72ae555701869f25683787e1
shalimmontes <shalimmontesh@gmail.com>
2025-01-24 13:11:21 -0600

    Added unit tests for is_disaster

--- Tests/test_cl.py
+++ Tests/test_cl.py
@@ -1,3 +1,17 @@
 import unittest, subprocess
 
 from ProductionCode.helper import *
+
+class ProjectMethodsTests(unittest.TestCase):
+    def test_is_disaster(self):
+        test1 = is_disaster("Rice")
+        self.assertEqual(test1, "True")
+
+        edgetest1 = is_disaster("rIcE")
+        self.assertEqual(edgetest1, "True")
+
+        test2 = is_disaster("Potato")
+        self.assertEqual(test2, "False")
+
+        edgetest2 = is_disaster(4)
+        self.assertEqual(edgetest2, "False")
