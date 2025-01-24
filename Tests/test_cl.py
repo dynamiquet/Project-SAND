a6e15a3dd3b16304103189f1f21a3e5354fb19a0
shalimmontes <shalimmontesh@gmail.com>
2025-01-24 13:16:21 -0600

    Changed tests, original tests were for is_us_county instead of is_disaster

--- Tests/test_cl.py
+++ Tests/test_cl.py
@@ -4,14 +4,11 @@ from ProductionCode.helper import *
 
 class ProjectMethodsTests(unittest.TestCase):
     def test_is_disaster(self):
-        test1 = is_disaster("Rice")
+        test1 = is_disaster("tornado")
         self.assertEqual(test1, "True")
 
-        edgetest1 = is_disaster("rIcE")
+        edgetest1 = is_disaster("TORNADO")
         self.assertEqual(edgetest1, "True")
 
-        test2 = is_disaster("Potato")
+        test2 = is_disaster("United States")
         self.assertEqual(test2, "False")
-
-        edgetest2 = is_disaster(4)
-        self.assertEqual(edgetest2, "False")
