201cc80c130a75dfabc6f3a31ee4438c643dd5b2
Francisco <farenas9134@gmail.com>
2025-01-29 16:59:03 -0600

    added my version

--- flask_app.py
+++ flask_app.py
@@ -30,3 +30,25 @@ def get_disaster_risk_flsk(disasters, county):
         return get_disaster_risk(disasters, county)
     else:
         return "At least one disaster and/or the county are not valid. Please check spelling and try again. For information on formatting, check the homepage."
+    
+# Francisco's version:
+# Error message for when browser arguments are invalid
+ErrorMessage = "Either your county or disaster are not valid inputs. Please check homepage to see correct usage."
+
+def check_valid_disasters_and_county(disaster, county):
+    '''Arguments: String of disaster(s), String of county
+    Return: Boolean
+    Purpose: To check if given disasters and county are valid'''
+
+    return (is_disaster(disaster) and is_us_county(county))
+
+@app.route('/<disaster>/<county>', strict_slashes = False)
+def get_valid_county_and_disaster(disaster, county):
+    '''Arguments: String of disaster(s), String of county
+    Return: List of disaster's
+    Purpose: To get disaster's hazard ratings in a county'''
+
+    if (check_valid_disasters_and_county(disaster, county)):
+        return get_disaster_risk(disaster, county)
+    
+    return ErrorMessage
