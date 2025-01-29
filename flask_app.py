67c1d6173a8022b8f3cf9ed202e64949bfe676e3
Francisco <farenas9134@gmail.com>
2025-01-29 16:54:07 -0600

    added homepage

--- flask_app.py
+++ flask_app.py
@@ -1,2 +1,21 @@
 from flask import Flask
 from ProductionCode.helper import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    '''Arguments: None
+    Return: String
+    Purpose: To provide instructions on how to use homepage'''
+
+    message = '''Welcome to the homepage for Project S.A.N.D.!<br> <br> 
+    In order to use this website concatenate arguments into the browser with ' / ' followed by either: 
+    <br><br> Usage One: &lt name of disaster(s) seperated by commas &gt / &lt name of US county, State abbreviation &gt <br>
+    Example: (existing url)/Tornado,Earthquake/Los Angeles,CA <br> <br>
+    This returns the hazard rating for the type of disaster(s) in the given county as recorded by FEMA's dataset<br><br>
+    Usage Two: top5/ &lt name of US county, State abbreviation &gt <br>
+    Example: (existing url)/top5/Rice,MN <br> <br>
+    This returns the top five most hazardous disaster's in a given county
+    '''
+    return message
