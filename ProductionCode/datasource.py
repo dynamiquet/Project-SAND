9a895b21d336c8a0911d1d63b38542220189f52d
Dynamique Twizere <twizeredynamique@gmail.com>
2025-02-12 17:14:49 -0600

    just made the docstrings collapsable

--- ProductionCode/datasource.py
+++ ProductionCode/datasource.py
@@ -5,12 +5,16 @@ from ProductionCode.helper import *
 class DataSource:
 
     def __init__(self):
-        '''Constructor that initiates connection to database'''
+        '''
+        Constructor that initiates connection to database
+        '''
         self.connection = self.connect()
 
     def connect(self):
-        '''Initiates connection to database using information in the psqlConfig.py file.
-        Returns the connection object.'''
+        '''
+        Initiates connection to database using information in the psqlConfig.py file.
+        Returns the connection object.
+        '''
 
         try:
             connection = psycopg2.connect(database=config.database, user=config.user, password=config.password, host="localhost")
@@ -40,7 +44,8 @@ class DataSource:
             return None
     
     def getRiskValuesbyCounty(self, disasters, county, state):
-        ''' Arguments: disasters (string), county (string), state (string)
+        ''' 
+        Arguments: disasters (string), county (string), state (string)
         Executes a SQL query by taking in the disasters list which are the columns of the database and retrieving the risk values for the specified US county which are then
         displayed 
         '''
@@ -64,7 +69,8 @@ class DataSource:
             return None
     
     def getCountyRow(self, county, state):
-        ''' Arguments: county (string), state (string)
+        ''' 
+        Arguments: county (string), state (string)
         Modify
         '''
         try:
