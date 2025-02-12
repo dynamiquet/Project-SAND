7c1fb5c308125cfe17739087016f943c96ab17d5
shalimmontes <shalimmontesh@gmail.com>
2025-02-12 17:09:10 -0500

    Added minor changes to other files and now checking for valid us county involves running a query

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from ProductionCode.datasource import DataSource
 #A simple file to just demonstrate the DataSource functionality
 test = DataSource()
 test.connect()
-print(test.getRiskValuesbyCounty("TORNADO, HURRICANE, EARTHQUAKE", "Rice", "MN"))
+print(test.getRiskValuesbyCounty("TORNADO, HURRICANE, EARTHQUAKE", "Rice", "MN")) 
