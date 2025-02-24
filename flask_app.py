e465eaba9267912fb3f8e5b65db1827752c682fc
arenasf <arenasf@carleton.edu>
2025-02-24 17:25:16 -0500

    added error display page

--- flask_app.py
+++ flask_app.py
@@ -11,14 +11,15 @@ def page_not_found(e):
     '''Argument(s): 404 error
     Displays a usage statement to the user if what follows the URL is formatted incorrectly
     '''
-    return "Page not found: Please ensure you follow the URL with: /&ltdisasters&gt/&ltcounty, state abbr.&gt or /&ltrow&gt/&ltcolumn&gt"
+    return render_template('displayerror.html')
 
 @app.errorhandler(500)
 def python_bug(e):
    '''Arguments: None
     Return: String of instructions
     Purpose: In case of developer error, directs users back to homepage'''
-   return "Sorry for the error, we have a bug in our code! Please go back to the homepage!" 
+   #"Sorry for the error, we have a bug in our code! Please go back to the homepage!" 
+   return render_template('displayerror.html')
 
 @app.route('/prevhome')
 def prevhomepage():
