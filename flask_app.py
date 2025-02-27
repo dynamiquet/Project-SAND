c791a86ba15c38c0db7df9921e951f296ab94ac0
shalimmontes <shalimmontesh@gmail.com>
2025-02-27 12:29:52 -0500

    Added a fully functioning dynamic dropdown menu for US states and counties

--- flask_app.py
+++ flask_app.py
@@ -84,7 +84,7 @@ def get_top5_disasters_for_county(county):
 @app.route('/displaycountydata')
 def display_county_disaster_data():
     requested_county = str(request.args['county'])
-    requested_state = str(request.args['state'])
+    requested_state = str(request.args['stateabbrv'])
     requested_disasters_list = str(request.args.getlist('hiddenSelectedDisaster'))[2:-2]
     risk_values_for_disasters_dictionary = test.getRiskValuesbyCounty(requested_disasters_list, requested_county, requested_state)
     return render_template('displaydata.html', results = risk_values_for_disasters_dictionary, state = requested_state, county=requested_county, data=risk_values_for_disasters_dictionary)
