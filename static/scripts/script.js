07807dec6f22ea56a02b690f30ff87943383b272
arenasf <arenasf@carleton.edu>
2025-02-27 13:24:34 -0500

    more stuff

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -30,4 +30,31 @@ function showPosition(position) {
     let user_latitude = position.coords.latitude
     let user_longitude = position.coords.longitude
     text.textContent = "Your Coordinates are: Latitude: " + user_latitude + "Longitude: " + user_longitude
+    initMap(user_latitude, user_longitude)
+}
+
+// Initialize and add the map
+let map;
+
+async function initMap(user_latitude, user_longitude) {
+  // The location of the User
+  const position = { lat: user_latitude, lng: user_longitude };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("GoogleMap"), {
+    zoom: 10,
+    center: position,
+    mapId: "GoogleMap",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Your Location",
+  });
 }
