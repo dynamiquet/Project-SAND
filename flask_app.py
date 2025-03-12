23ec486bd868cc9b4f6b4d63bd67c2214022831c
Dynamique Twizere <twizeredynamique@gmail.com>
2025-03-11 23:48:09 -0400

    about page

--- flask_app.py
+++ flask_app.py
@@ -124,8 +124,7 @@ def display_resources_page():
 
 @app.route('/about')
 def display_about_me_page():
-    return render_template('about.html')
-
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run()
