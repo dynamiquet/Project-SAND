b5910e0c8cc3c5e565ed6366b2505720c66a9904
arenasf <arenasf@carleton.edu>
2025-03-12 15:19:12 -0400

    made all collapsibles be active by default

--- static/scripts/CollapsibleElements.js
+++ static/scripts/CollapsibleElements.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function setCollapsibles() {
     Purpose: Waits for a button with class "collapsible" to be clicked on and displays content
     */
     var allCollapsibles = document.querySelectorAll(".collapsible");
+    setAllCollapsiblesActive();
 
     allCollapsibles.forEach(function (button) { // Loop through each collapsible button
         button.addEventListener("click", function () {
@@ -20,3 +21,19 @@ document.addEventListener("DOMContentLoaded", function setCollapsibles() {
         });
     });
 });
+
+function setAllCollapsiblesActive () {
+    var allCollapsibles = document.querySelectorAll(".collapsible");
+
+    allCollapsibles.forEach(function (button) {
+        button.classList.toggle("active");
+        
+        var content = button.nextElementSibling;
+        
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    })
+};
