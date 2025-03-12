b36663244fed6887b415f19ec52a15d2b027b0df
FlorisdeBRuxelles <irakozefioris2542@gmail.com>
2025-03-11 21:35:42 -0500

    modified the flask to connect with the about us page

--- flask_app.py
+++ flask_app.py
@@ -124,7 +124,8 @@ def display_resources_page():
 
 @app.route('/about')
 def display_about_me_page():
-    return "Page describing who we are and what our mission is."
+    return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run()
